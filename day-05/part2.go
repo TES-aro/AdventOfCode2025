5a25021e307f8195d1aea7cf54ac85e3a4e1b717
TES <taaviaro@student.uef.fi>
2025-12-12 08:50:25 +0200

    can't think

--- day-05/part2.go
+++ day-05/part2.go
@@ -1,32 +1,46 @@
-package main
-import
 
-func part2(buffer *buffio.)
 package main
 
 import (
 	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 	"strings"
 )
 // guess I'll implement linked list meself
-type LinkedList struct{
-	head  *Node
-	tail *Node
-	current *Node
+type IDLinkedList struct{
+	head  *IDNode
+	tail *IDNode
+	current *IDNode
 }
 
-type Node struct{
-	data int
-	next *Node
-	prev *Node
+type IDNode struct{
+	data IDrange
+	next *IDNode
+	prev *IDNode
 }
 
 // InsertAtEnd inserts a new node at the end of the list
-func (ll *LinkedList) Add(data int) {
-    newNode := &Node{data: data}
+func (node *IDNode) Delete(list *IDLinkedList){
+	prev := node.prev
+	next := node.next
+	if node == list.head {
+		list.head = node.next
+		list.head.prev = nil
+	}
+
+	if node == list.tail {
+		list.tail = node.prev
+		node.prev.next = nil
+	}
+	if prev != nil {
+		prev.next = next
+	}
+	if next != nil {
+		next.prev = prev
+	}
+}
+func (ll *IDLinkedList) Add(data IDrange) {
+    newNode := &IDNode{data: data}
     if ll.head == nil {
 	    fmt.Println("Started a new list!")
         // If the list is empty, set both head and tail to the new node
@@ -40,141 +54,136 @@ func (ll *LinkedList) Add(data int) {
     }
 }
 
-func (ll *LinkedList) Start() int{
-	ll.current = ll.head
-	return ll.current.data
-}
-func (ll *LinkedList) Next() (int, string){
-	if ll.current == nil{
-		return 0, "no next found"
-	}
-	val := ll.current.data
-	ll.current = ll.current.next
-	return val, ""
-}
-func (ll *LinkedList) Delete(){
-
-}
-
-func (ll *LinkedList) Display() {
-    current := ll.head
-    for current != nil {
-        fmt.Printf("%d -> ", current.data)
-        current = current.next
-    }
-    fmt.Println("nil")
-}
-
-
-/*
-func (ll LinkedList) Add(val int){
-	if(ll.end == nil){
-		newNode := &listNode{value: val}
-		ll.startNode = newNode
-		ll.endNode = newNode
-		return
-	}
-	newNode := &listNode{val, nil, ll.endNode}
-	endNode := ll.endNode
-	endNode.nextNode = newNode
-	ll.endNode = newNode
-}
-
-func (ln listNode) HasNext() bool{
-	if ln.nextNode != nil{
-		return true
-	}
-	return false
-}
-
-func (ln listNode) Remove(){
-	prev := ln.prevNode
-	next := ln.nextNode
-	prev.nextNode = next
-	next.prevNode = prev
-}
-*/
-
-
-
 // back to code proper
 
-func part1(scanner *bufio.Scanner){
-	var ranges []IDrange
-	items := *new(LinkedList)
+func part2(scanner *bufio.Scanner){
+	ranges := *new(IDLinkedList)
 	llCounter := 0;
 	for scanner.Scan(){
 		line := scanner.Text();
 		if line == "" {
 			fmt.Println("blank line detected")
-			for scanner.Scan(){
-				line := scanner.Text();
-				items.Add( toInt(line))
-				llCounter++
-			}
 			break
 		}
-		ranges = addIDrange(ranges, line)
+		llCounter++
+		hPair := strings.Split(line, "-")
+		var pair = IDrange{toInt(hPair[0]), toInt(hPair[1])}
+		ranges.Add(pair)
 	}
 	fmt.Println(llCounter)
-	compare(ranges, items)
+	iterate(ranges)
 }
 
-func compare(ranges []IDrange, items LinkedList){
-	freshCount := 0
-	for i:= 0; i < len(ranges); i++{
-		Range := ranges[i]
-		//fmt.Println("now handling range")
-		//fmt.Println(idRange)
-		node := items.head
+func fullOverlap (first IDrange, second IDrange) bool{
+	fStart := first.start
+	fEnd := first.end
+	sStart := second.start
+	sEnd := second.end
 
-		for node != nil{
-			val := node.data
-			if val <= Range.end && val >= Range.start{
-				freshCount++
-				nextNode := node.next
-				prevNode := node.prev
+	if fEnd < sEnd && fStart > sStart{
+		return true
+	}
+	return false
+}
 
-				if node == items.head {
-					items.head = node.next
-					items.head.prev = nil
-				}
+func overlapStart(first IDrange, second IDrange) bool{
+	fStart := first.start
+	sStart := second.start
+	sEnd := second.end
 
-				if node == items.tail {
-					items.tail = node.prev
-					node.prev.next = nil
+	if fStart <= sEnd && fStart >= sStart{
+		return true
+	}
+	return false
+}
+
+
+func overlapEnd(first IDrange, second IDrange) bool{
+	fStart := first.start
+	fEnd := first.end
+	sStart := second.start
+	sEnd := second.end
+
+	if fS§
+}	
+func overlap(first IDrange, second IDrange) bool{
+	fStart := first.start
+	fEnd := first.end
+	sStart := second.start
+	sEnd := second.end
+	if fEnd < sStart || sEnd < fStart{
+		return false
+	}
+	// delete these.
+	if fEnd < sEnd && fStart > sStart{
+		return true
+	}
+	if fEnd >= sStart && fStart < sStart{
+		return true
+	}
+	return false
+}
+
+func iterate(rangeList IDLinkedList){
+	for true {
+		delCounter := 0
+		nodePrime := rangeList.head
+		for nodePrime != nil {
+			pPair := nodePrime.data
+			pStart := pPair.start
+			pEnd := pPair.end
+			nodeSecond := rangeList.head
+			for nodeSecond != nil{
+				if nodeSecond == nodePrime{
+					nodeSecond = nodeSecond.next
+					continue
 				}
-				if(nextNode != nil){
-					nextNode.prev = prevNode
+				sPair := nodeSecond.data
+				sStart := sPair.start
+				sEnd := sPair.end
+				if sEnd <= pEnd && sStart >= pStart {
+					delCounter++
+					nodeSecond.Delete(&rangeList) 
 				}
-				if(prevNode != nil){
-					prevNode.next = nextNode
+				if (sStart <= pStart){
+					if(sEnd >= pStart){
+						nodePrime.data.start = sStart
+						if(sEnd >= pEnd){
+							nodePrime.data.end = sEnd
+						}
+						//deleting manually yay
+						delCounter++
+						nodeSecond.Delete(&rangeList)
+					}
 				}
+				if (sEnd >= pEnd && sStart <= pEnd){
+					nodePrime.data.end = sEnd;
+					delCounter++
+					nodeSecond.Delete(&rangeList)
+				}
+ 				nodeSecond = nodeSecond.next
 			}
-			node = node.next
+			nodePrime = nodePrime.next
+		}
+		fmt.Println(delCounter) 
+		if delCounter == 0 {
+			break
 		}
 	}
-	fmt.Println("answer is:")
-	fmt.Println(freshCount)
-}
-
-type IDrange struct {
-	start int
-	end int
-}
-func toInt(str string) int {
-	num, err := strconv.Atoi(str)
-	if err != nil {
-		fmt.Println("issue converting '" + str + "' to int")
-		os.Exit(42)
+	//once more!
+	node := rangeList.head
+	counter := 0
+	ingredientCounter := 0
+	for node != nil {
+		counter++
+		fmt.Print(node.data.start)
+		fmt.Print(" - ")
+		fmt.Println(node.data.end) 
+		ingredientCounter += (node.data.end - node.data.start + 1)
+		node = node.next
 	}
-	return num
+	fmt.Println("new list size:")
+	fmt.Println(counter)
+	fmt.Println("Answer is:")
+	fmt.Println(ingredientCounter) 
 }
-func addIDrange(array []IDrange, valuePair string) []IDrange{
-	helperPair := strings.Split(valuePair, "-")
-	var pair = IDrange{toInt(helperPair[0]), toInt(helperPair[1])}
-	array = append(array, pair)
-	return array
-}
-
-
